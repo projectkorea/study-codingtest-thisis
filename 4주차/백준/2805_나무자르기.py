60e512e9a2942b3c936ca7d339033239c09ad54a
Junha <prodopark15@gmail.com>
2021-11-05 10:52:22 +0900

    4w fri

--- 4주차/백준/2805_나무자르기.py
+++ 4주차/백준/2805_나무자르기.py
@@ -41,16 +41,63 @@ def BS_tree(data,M):
         for x,num in data.items():
           if x > mid:
             total += (x-mid)*num #해당 값이 등장한 개수만큼 곱한다
-
         if total < M:
             end = mid -1
         else:
             result = mid
             start = mid + 1
-
     return result
-
 
 _, M = map(int, stdin.readline().split())
 data = Counter(map(int, stdin.readline().split()))
 print(BS_tree(data,M))
+
+
+# 복습: 시간초과가 안나네? 3036 ms
+from sys import stdin
+
+def BS_Tree(li,m):
+    start,end = 0,max(li)
+    answer = 0
+    while start <= end:
+        mid = (start+end) //2
+        result = 0
+        for item in li:
+            if item > mid:
+                result+=item-mid
+        if result >= m:
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+    return answer
+    
+   
+_,m = map(int,stdin.readline().split())
+li = list(map(int,stdin.readline().split()))
+print(BS_Tree(li,m))
+
+# 복습: 카운터함수를 쓰니 시간이 급격히 줄어들었다 456 ms
+from sys import stdin
+from collections import Counter
+
+def BS_Tree(data,m):
+    start,end = 0,max(data)
+    answer = 0
+    while start <= end:
+        mid = (start+end) //2
+        result = 0
+        for item, num in data.items():
+            if item > mid:
+                result+=(item-mid) * num
+        if result >= m:
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+    return answer
+    
+   
+_,m = map(int,stdin.readline().split())
+data = Counter(map(int,stdin.readline().split()))
+print(BS_Tree(data,m))
